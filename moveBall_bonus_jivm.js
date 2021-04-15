a45520066cc886fc233943b78deaa5f42d443a53
Jorge I. Vargas M <63824323+jivm-mx@users.noreply.github.com>
2021-04-15 01:21:01 -0500

    Update moveBall_bonus_jivm.js
    Trabajar con objetos

--- moveBall_bonus_jivm.js
+++ moveBall_bonus_jivm.js
@@ -1,89 +1,145 @@
-/*Constantes para que establecen los valores máximo y mínimos para cambio de color, velocidad, intervalo de tiempo, radio e indice Z*/
-const rgbMin = 0;
-const rgbMax = 256;
-/*Cantidad minima y máxima (excluyente) de pixeles que se sumaran a la posición horizontal o vertical*/
-const minVel = 10;
-const maxVel = 31;
-/*Valor de radio mínimo y máximo (excluyente) para cambio de forma */
-const minRadio = 0;
-const maxRadio = 101;
-/*Valor de radio mínimo y máximo (excluyente) para cambio de profundidad (indice Z) */
-const minIndiceZ = 0;
-const maxIndiceZ = 501;
+/*Set limits */
+const limits = {
+  width: 600,
+  height: 480,
+};
+/*Initial properties */
+const objectProps = {
+  id: 0,
+  zIndex: 0,
+  left: 0,
+  top: 50,
+  width: 50,
+  height: 50,
+  borderRadius: 50,
+  background: [0, 0, 0],
+  speedLeft: 50, //cannot be 0
+  speedTop: 50, //cannot be 0
+  goForwardLeft: true,
+  goForwardTop: true,
+};
 
-/*Hay que conocer el tamaño de la bola y con base en eso, establecer los límites*/
-/*Parámetros no configurables*/
-var bola = document.getElementById("bola");
-var direccionX = 1;
-var direccionY = 1;
-var bolaId = 0;
-var indiceZ = 0;
-var rgb = [0, 0, 0];
+const leftEnd = limits.width - objectProps.width;
+const leftStart = objectProps.left;
 
+const topEnd = limits.height - objectProps.height;
+const topStart = objectProps.top;
+
+
+/*Parameters*/
+const time = 100; //bounce interval
+const duplicateTime = 1000; //duplicate interval
+const maxRepetitions = 1000;
+var repetitions = 0; 
+var bounceInterval;
+var duplicateInterval;
+
+object = document.getElementById("object");
 /* Parámetros configurables*/
-var velocidadX = 1;
-var velocidadY = 1;
-/*Tiempo para intervalos */
-var tiempo = 20;
-var tiempoduplicado = 1000;
 
-const bolaAncho = bola.style.width.substring(0, bola.style.width.length - 2);
-const bolaAlto = bola.style.height.substring(0, bola.style.height.length - 2);
-/*límites para valores left (anchoMaximo) y top (altoMaximo)*/
-var anchoMaximo = 800;
-var altoMaximo = 600;
-const limiteDerecho = anchoMaximo - bolaAncho;
-const limiteInferior = altoMaximo - bolaAlto;
-const limiteIzquierdo = Number(bola.style.left.substring(0, bola.style.left.length - 2));
-const limiteSuperior = Number(bola.style.top.substring(0, bola.style.top.length - 2));
-const radioInicial = Number(bola.style.borderRadius.substring(0, bola.style.borderRadius.length - 1));
-/*Posiciones iniciales que dependen de los limites */
-var posicionX = limiteIzquierdo;
-var posicionY = limiteSuperior;
-var radio = radioInicial;
 /*
 Al tocar límite derecho, cambia el color de la bola, al tocar el límite izquierdo cambia la velocidad, al todar el límite inferior, cambia el radio y al tocar el límite superior, cambia el índice Z. Hay dos intervalos, uno para actualizar la posición de la bola y otro para realizar una copia y preservar ese estado en pantalla.
 Por hacer: detener la ejecución y modificar dinámicamente los intervalos,
 */
 
-function rebota() {
-  // console.log("posicion actual (x,y):" + posicionX + "," + posicionY);
-  document.getElementById("posicionX").innerHTML = posicionX;
-  document.getElementById("posicionY").innerHTML = posicionY;
-  document.getElementById("velocidadX").innerHTML = velocidadX;
-  document.getElementById("velocidadY").innerHTML = velocidadY;
-  document.getElementById("direccionX").innerHTML = direccionX;
-  document.getElementById("direccionY").innerHTML = direccionY;
-  document.getElementById("indiceZ").innerHTML = indiceZ;
-  document.getElementById("radio").innerHTML = radio;
+function start() {
+  console.log("Hay que esperar que inicie la grabación del video");
+  bounceInterval = setInterval(bounce, time);
+  duplicateInterval = setInterval(duplicate, duplicateTime);  
+}
+
+function stopIntervals(){
+  if(repetitions > maxRepetitions){
+    clearInterval(bounceInterval);
+    clearInterval(duplicateInterval);
+    console.log("Finished")
+  }
   
+}
+setTimeout(start, 1500);
 
-  posicionX += velocidadX * direccionX;
-  posicionY += velocidadY * direccionY;
+function bounce() {
+  //debugObjectProps();
+  move();
+  checkBoundaries();
+  bonus();
+  updateStatistics();
+  stopIntervals();
+  repetitions ++;
+  object.style.left = objectProps.left + "px";
+  object.style.top = objectProps.top + "px";
+}
 
-  if (posicionX >= limiteDerecho) {
-    posicionX = limiteDerecho;
-    direccionX = -1;
-    cambiaColor();
+function move() {
+  let directionLeft = objectProps.goForwardLeft ? 1 : -1;
+  let directionTop = objectProps.goForwardTop ? 1 : -1;
+
+  objectProps.left += objectProps.speedLeft * directionLeft;
+  objectProps.top += objectProps.speedTop * directionTop;
+}
+
+function checkBoundaries() {
+  if (objectProps.left >= leftEnd) {
+    objectProps.left = leftEnd;
+    objectProps.goForwardLeft = false;
   }
-  if (posicionX <= limiteIzquierdo) {
-    posicionX = limiteIzquierdo;
-    direccionX = 1;
-    cambiaVelocidad();
-  }
-  if (posicionY >= limiteInferior) {
-    posicionY = limiteInferior;
-    direccionY = -1;
-    cambiaForma();
-  }
-  if (posicionY <= limiteSuperior) {
-    posicionY = limiteSuperior;
-    direccionY = 1;
-    cambiaIndiceZ();
+  if (objectProps.left <= leftStart) {
+    objectProps.left = leftStart;
+    objectProps.goForwardLeft = true;
   }
 
-  bola.style.left = posicionX + "px";
-  bola.style.top = posicionY + "px";
+  if (objectProps.top >= topEnd) {
+    objectProps.top = topEnd;
+    objectProps.goForwardTop = false;
+  }
+  if (objectProps.top <= topStart) {
+    objectProps.top = topStart;
+    objectProps.goForwardTop = true;
+  }
+}
+
+function bonus() {
+  if (objectProps.left === leftEnd) {
+    changeColor(0, 256);
+  }
+  if (objectProps.left === leftStart) {
+    changeborderRadius(0, 101);
+  }
+  if (objectProps.top == topEnd) {
+    changeZIndex(1, 21);
+  }
+  if (objectProps.top === topStart) {
+    changeSpeed(50, 200);
+  }
+}
+
+function changeColor(min, max) {
+  objectProps.background[0] = getRandomInt(min, max);
+  objectProps.background[1] = getRandomInt(0, 256);
+  objectProps.background[2] = getRandomInt(0, 256);
+
+  object.style.background =
+    "rgb(" +
+    objectProps.background[0] +
+    "," +
+    objectProps.background[1] +
+    "," +
+    objectProps.background[2] +
+    ")";
+}
+
+function changeborderRadius(minBorder, maxBorder) {
+  objectProps.borderRadius = getRandomInt(minBorder, maxBorder);
+  object.style.borderRadius = objectProps.borderRadius + "%";
+}
+function changeZIndex(minZ, maxZ) {
+  objectProps.zIndex = getRandomInt(minZ, maxZ);
+  object.style.zIndex = objectProps.zIndex;
+}
+
+function changeSpeed(minVel, maxVel) {
+  objectProps.speedLeft = getRandomInt(minVel, maxVel);
+  objectProps.speedTop = getRandomInt(minVel, maxVel);
 }
 
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -95,56 +151,39 @@ function getRandomInt(min, max) {
 
 //https://stackoverflow.com/questions/4427094/how-can-i-duplicate-a-div-onclick-event
 function duplicate() {
-  var clone = bola.cloneNode(true); // "deep" clone
-  clone.id = "bola" + ++bolaId;
+  const clone = object.cloneNode(true); // "deep" clone
+  clone.id += " " + objectProps.id;
+  object.parentNode.appendChild(clone);
+  objectProps.id += 1;
+  //clone.id = "bola" + bolaId;
   // or clone.id = ""; if the divs don't need an ID
-  bola.parentNode.appendChild(clone);
+  //bola.parentNode.appendChild(clone);
 }
 
-function cambiaColor() {
-  rgb[0] = getRandomInt(rgbMin, rgbMax);
-  rgb[1] = getRandomInt(rgbMin, rgbMax);
-  rgb[2] = getRandomInt(rgbMin, rgbMax);
-  bola.style.background = "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
+function updateStatistics() {
+  document.getElementById("left").innerHTML = objectProps.left;
+  document.getElementById("top").innerHTML = objectProps.top;
+  document.getElementById("speedLeft").innerHTML = objectProps.speedLeft;
+  document.getElementById("speedTop").innerHTML = objectProps.speedTop;
+  document.getElementById("goForwardLeft").innerHTML = objectProps.goForwardLeft;
+  document.getElementById("goForwardTop").innerHTML = objectProps.goForwardTop;
+  document.getElementById("zIndex").innerHTML = objectProps.zIndex;
+  document.getElementById("borderRadius").innerHTML = objectProps.borderRadius;
 }
 
-function cambiaVelocidad() {
-  velocidadX = getRandomInt(minVel, maxVel);
-  velocidadY = getRandomInt(minVel, maxVel);
-}
-
-function cambiaForma() {
-  radio = getRandomInt(minRadio, maxRadio);
-  bola.style.borderRadius = radio + "%";
-}
-
-function cambiaIndiceZ() {
-  indiceZ = getRandomInt(minIndiceZ, maxIndiceZ);
-  bola.style.zIndex = indiceZ;
-}
-function debug(){
+function debugObjectProps() {
   console.log(
-    "posicion nueva (x,y):" +
-      posicionX +
+    "(x,y): " +
+      objectProps.left +
       "," +
-      posicionY +
-      " color:" +
-      rgb +
-      ", velocidadX: " +
-      velocidadX +
-      ", velocidadY: " +
-      velocidadY +
-      ", radio: " +
-      radio +
-      "%, indice-z: " +
-      indiceZ
+      objectProps.top +
+      " ,speedLeft: " +
+      objectProps.speedLeft +
+      " , speedTop: " +
+      objectProps.speedTop +
+      ", goForwardLeft: " +
+      objectProps.goForwardLeft +
+      ", goForwardTop: " +
+      objectProps.goForwardTop
   );
 }
-
-function retraso() {
-  console.log("Hay que esperar que inicie la grabación del video");
-  setInterval(rebota, tiempo);
-  setInterval(duplicate, tiempoduplicado);
-}
-
-setTimeout(retraso, 1500);
