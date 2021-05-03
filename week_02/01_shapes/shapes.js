c79aaaedbf033af576487556daa0ef277ac365eb
jivm-mx <jisra.vargas@gmail.com>
2021-05-03 01:47:26 -0500

    Ejercicios semana 2

--- week_02/01_shapes/shapes.js
+++ week_02/01_shapes/shapes.js
@@ -13,14 +13,18 @@ function create(id, zIndex, left, top, color) {
 }
 
 function First() {
-  create(0, 0, 200, 200, [139, 0, 0]); // darkred
-  create(1, 2, 150, 250, [125, 200, 125]); // green
-  create(2, 2, 250, 250, [210, 105, 30]); // orange
-  create(3, 1, 100, 300, [210, 105, 30]); // orange
-  create(4, 3, 150, 350, [100, 200, 255]); // blue
-  create(5, 1, 250, 350, [150, 150, 150]); // gray
-  create(6, 2, 200, 400, [139, 0, 0]); // darkred
-  create(7, 0, 300, 300, [100, 200, 255]); // blue
+  create('0', 0, 200, 200, [139, 0, 0]); // darkred
+
+  create('1a', 2, 150, 250, [125, 200, 125]); // green
+  create('1b', 2, 250, 250, [210, 105, 30]); // orange
+
+  create('2a', 1, 100, 300, [210, 105, 30]); // orange
+  create('2b', 0, 300, 300, [100, 200, 255]); // blue
+
+  create('3a', 3, 150, 350, [100, 200, 255]); // blue
+  create('3b', 1, 250, 350, [150, 150, 150]); // gray
+
+  create('4', 2, 200, 400, [139, 0, 0]); // darkred
 }
 
 function startExercises() {
